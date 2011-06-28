e61a219b4bc7b5dc58a23c66ea561f53b8e4b386
Norman Xu <homyu.shinn@gmail.com>
2011-06-28 18:59:20 +0800

    update docs js

---
+++ js/docs.js
@@ -0,0 +1,23 @@
+!function(){
+	
+	var homeController = etui.ctor(function(){
+		
+	}, etui.mvc.controller);
+	
+	homeController
+	
+	var controllers = {
+		homeController: new etui.mvc.controller()
+	}
+	
+	function registerController(){
+		
+	}
+	
+	var docsApp = function(){
+	};
+	
+	var p = docsApp.prototype;
+	
+	
+}();
